1cefb1acf46e627a30df1609789714a759a7eeec
zimu-java-python <2869758975@qq.com>
2026-03-05 13:10:58 +0800

    用AI新增注释

--- java-basic/src/com/itheima/hello/HelloWorld.java
+++ java-basic/src/com/itheima/hello/HelloWorld.java
@@ -1,6 +1,15 @@
 package com.itheima.hello;
 
+/**
+ * HelloWorld 程序入口类
+ * 该类包含 Java 程序的入口方法，用于输出欢迎信息
+ */
 public class HelloWorld {
+    /**
+     * Java 程序的主入口方法
+     * 
+     * @param args 命令行参数数组，字符串类型，用于接收程序启动时传入的参数
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
